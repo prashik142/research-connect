5ff8bf68a164bdc4b0c2b14d1372c453a75baaba
prash1412 <prashikmane1412@gmail.com>
2026-06-27 15:22:27 +0530

    Fix Render port

--- client/src/server.js
+++ client/src/server.js
@@ -19,9 +19,7 @@ app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
   res.send("ResearchConnect Backend Running");
 });
-console.log("process.env.PORT =", process.env.PORT);
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
--- server/src/server.js
+++ server/src/server.js
@@ -37,6 +37,8 @@ app.use(
     path.join(__dirname, "../uploads")
   )
 );
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
